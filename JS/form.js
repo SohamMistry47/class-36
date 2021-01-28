11b2d0c38ecb66955bfb82b4872b44cba128d063
SohamMistry47 <78021003+SohamMistry47@users.noreply.github.com>
2021-01-28 15:57:15 +0530

    Add files via upload

---
+++ JS/form.js
@@ -0,0 +1,24 @@
+class Form {
+
+    constructor(){
+
+    }
+
+    display(){
+        var title = createElement('h2');
+        title.html("Car Race");
+        title.position(250, 0);
+
+        var imput = createImput("name");
+        imput.position(200, 100);
+        
+        var button = createButton("PLAY");
+        button.position(200, 150);
+
+        var greeting = createElement('h3');
+    }
+
+
+
+
+}
